4dccb8edb99075fce6de0eccf75aa93f429320a6
dtrvno <dimatru@gmail.com>
2021-04-01 11:53:12 -0700

    added java configuration

--- src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -1,13 +1,24 @@
 package guru.springframework.sfgdi.config;
 
-import guru.springframework.sfgdi.services.ConstructorGreetingService;
-import guru.springframework.sfgdi.services.PropertyInjectedGreetingService;
-import guru.springframework.sfgdi.services.SetterInjectedGreetingService;
+import guru.springframework.sfgdi.services.*;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+
 @Configuration
 public class GreetingServiceConfig {
+    @Profile("EN")
+    @Bean("i18nService")
+    I18nEnglishGreetingService i18nService() {
+        return new I18nEnglishGreetingService();
+    }
+    @Profile({"ES","default"})
+    @Bean("i18nService")
+    I18NSpanishService i18NSpanishService() {
+        return new I18NSpanishService();
+    }
     @Bean
     ConstructorGreetingService constructorGreetingService() {
         return new ConstructorGreetingService();
@@ -19,5 +30,10 @@ public class GreetingServiceConfig {
     @Bean
     SetterInjectedGreetingService setterInjectedGreetingService() {
         return new SetterInjectedGreetingService();
+    }
+    @Primary
+    @Bean
+    PrimaryGreetingService primaryGreetingService() {
+        return new PrimaryGreetingService();
     }
 }
