c1fc6399a476b34f796877644bb88137d5521482
dtrvno <dimatru@gmail.com>
2021-04-01 11:39:57 -0700

    added java configuration

---
+++ src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -0,0 +1,23 @@
+package guru.springframework.sfgdi.config;
+
+import guru.springframework.sfgdi.services.ConstructorGreetingService;
+import guru.springframework.sfgdi.services.PropertyInjectedGreetingService;
+import guru.springframework.sfgdi.services.SetterInjectedGreetingService;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
+@Configuration
+public class GreetingServiceConfig {
+    @Bean
+    ConstructorGreetingService constructorGreetingService() {
+        return new ConstructorGreetingService();
+    }
+    @Bean
+    PropertyInjectedGreetingService propertyInjectedGreetingService() {
+       return new PropertyInjectedGreetingService();
+    }
+    @Bean
+    SetterInjectedGreetingService setterInjectedGreetingService() {
+        return new SetterInjectedGreetingService();
+    }
+}
